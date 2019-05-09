7cd24608e7de4403894f9aa73a30683dbb494baa
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-09 16:38:01 -0400

    building out html

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'hello, world'
+def home():
+    return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
