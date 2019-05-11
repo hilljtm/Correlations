9b742c195393502415b8405735ecff8a9cb84c0d
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-11 18:40:06 -0400

    doc strings for btc file started

--- btc.py
+++ btc.py
@@ -4,6 +4,7 @@ import requests
 import datetime
 import pandas as pd
 
+#using cryptocompare API to get historical btc data
 url = 'https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10'
 r = requests.get(url)
 data = r.json()
