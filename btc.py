dd9ca9ddb59eeef95854d573c2d1c231a17b773e
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-13 17:51:23 -0400

    plot for btc made, ran into issues using other api's, working on solution to get spx data

--- btc.py
+++ btc.py
@@ -1,10 +1,16 @@
 # %%
-import quandl 
+import quandl
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns; sns.set()
+import seaborn as sns
+sns.set()
 
-quandl.ApiConfig.api_key = 'API_KEY'
+#using quandl API, returning only last price
+quandl.ApiConfig.api_key = 'W6U4YhAz2q7bigp7tQge'
 data = quandl.get("BITFINEX/BTCUSD", start_date="2016-01-01")
-data = data[["Last"]] 
+data = data[["Last"]]
+pd.DataFrame(data, columns=['Last'])
+plt.rcParams["figure.figsize"] = [16,9]
+plt.plot(data)
+
