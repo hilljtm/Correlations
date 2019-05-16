f7e3624469cfe2bbe31f4c53e954dbe9c02d2e12
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-16 16:20:36 -0400

    concat dataframes in gold, and btc but gold giving NaN, think it's due to the weekend data not being avaliable for gold. df.dropna needed, added config file so i don't keep pushing my key

--- btc.py
+++ btc.py
@@ -1,4 +1,5 @@
 # %%
+import config
 import quandl
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ import seaborn as sns
 sns.set()
 
 #using quandl API, returning only last price
-quandl.ApiConfig.api_key = 'YOUR_KEY'
+quandl.ApiConfig.api_key = config.api_key
 data = quandl.get("BITFINEX/BTCUSD", start_date="2016-01-01")
 data = data[["Last"]]
 pd.DataFrame(data, columns=['Last'])
--- gold.py
+++ gold.py
@@ -1,4 +1,5 @@
 #%%
+import config
 import quandl
 import pandas as pd
 import numpy as np
@@ -6,27 +7,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-quandl.ApiConfig.api_key = 'W6U4YhAz2q7bigp7tQge'
+quandl.ApiConfig.api_key = config.api_key
 
-
-gold = quandl.get("WGC/GOLD_DAILY_USD", start_date='2016-01-05', end_date='2019-05-10')
-btc = quandl.get("BITFINEX/BTCUSD", start_date='2016-01-05', end_date='2019-05-10')
+gold = quandl.get("WGC/GOLD_DAILY_USD", start_date='2016-01-05')
+btc = quandl.get("BITFINEX/BTCUSD", column_index='1', start_date='2016-01-05')
 df1 = pd.DataFrame(data=gold)
-df = pd.DataFrame(data=btc, columns=['High'])
+df = pd.DataFrame(data=btc)
 
-plt.subplot(2, 1, 1)
-plt.plot(df1.index, df1)
-
-plt.xticks(df1.index[0::36], [])
-
-
-plt.title('Gold vs BTC')
-plt.ylabel('% Gold')
-plt.subplot(2, 1, 2)
-plt.plot(df.index, df)
-plt.yticks(df.index[0::100], [])
-
-plt.xlabel('year')
-plt.ylabel('% BTC')
-#plt.savefig('gold_btc.png', facecolor='w', edgecolor='w', bbox_inches='tight')
-plt.show()
+horizontal_stack = pd.concat([df1, df], axis=1)
+print(horizontal_stack) 
