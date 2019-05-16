8ad877463be34f6569d88afcad00046f84404e0b
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-16 12:07:42 -0400

    added plt back in, need to format and possibly reconsider using seaborn

--- gold.py
+++ gold.py
@@ -14,3 +14,19 @@ btc = quandl.get("BITFINEX/BTCUSD", start_date='2016-01-05', end_date='2019-05-1
 df1 = pd.DataFrame(data=gold)
 df = pd.DataFrame(data=btc, columns=['High'])
 
+plt.subplot(2, 1, 1)
+plt.plot(df1.index, df1)
+
+plt.xticks(df1.index[0::36], [])
+
+
+plt.title('Gold vs BTC')
+plt.ylabel('% Gold')
+plt.subplot(2, 1, 2)
+plt.plot(df.index, df)
+plt.yticks(df.index[0::100], [])
+
+plt.xlabel('year')
+plt.ylabel('% BTC')
+#plt.savefig('gold_btc.png', facecolor='w', edgecolor='w', bbox_inches='tight')
+plt.show()
