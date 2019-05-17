9ea2864918c6ad05b734d2a320fca125186e6d98
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-17 11:59:45 -0400

    sp500 data cleaned, plot layout started just needs formatted

--- spindex.py
+++ spindex.py
@@ -2,8 +2,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib as mpl
+plt.style.use('seaborn-whitegrid')
+import seaborn
 
-sp_df = pd.read_csv("spindex.csv")
-df = pd.DataFrame(sp_df)
-df
+
+
+df = pd.read_csv("sp500.csv")
+
+x = df['Date']
+y = df['Price']
+
+plt.plot(x, y)
+plt.grid(color='gray', linestyle='solid')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
+
