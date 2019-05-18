36271703177b8aca65a54adf14a3a75dc1bb08ef
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-18 12:19:07 -0400

    sp500 & btc plot made and good to go, not sure i need to concat

--- gold.py
+++ gold.py
@@ -15,14 +15,22 @@ btc = quandl.get("BITFINEX/BTCUSD", column_index='1', start_date='2016-01-05')
 df1 = pd.DataFrame(data=gold)
 df = pd.DataFrame(data=btc)
 
-horizontal_stack = pd.concat([df1, df], axis=1)
-print(horizontal_stack)
 
-fig = plt.figure()
-ax = plt.axis()
+# horizontal_stack = pd.concat([df1, df], axis=1) --> not sure this is even needed
 
-plt.rcParams["figure.figsize"] = [16, 9]
-plt.title("GOLD&BTC")
-plt.xlabel("Date")
-plt.ylabel("$Price")
+plt.subplot(2, 1, 1)
+plt.plot(gold.index, gold)
+
+#TODO find out how to make graphs match, solve the NaN issue
+plt.xticks(gold.index[0::85], [])
+
+
+plt.title('Gold vs BTC')
+plt.ylabel('% Gold')
+plt.subplot(2, 1, 2)
+plt.plot(btc.index, btc)
+
+plt.xlabel('year')
+plt.ylabel('% BTC')
+plt.savefig('gold_btc.png', facecolor='w', edgecolor='w', bbox_inches='tight')
 plt.show()
--- spindex.py
+++ spindex.py
@@ -1,22 +1,40 @@
 #%%
+import config
+import quandl 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import seaborn as sns
 
-fig = plt.figure()
-ax = plt.axis()
+quandl.ApiConfig.api_key = config.api_key
 
-data = pd.read_csv("sp500.csv")
-df = pd.DataFrame(data)
 
-x = df['Date']
-y = df['Price']
+btc = quandl.get("BITFINEX/BTCUSD", column_index='1', start_date='2016-01-05')
+df1 = pd.DataFrame(btc)
+data = pd.read_csv("sp500.csv", index_col=0)
+sp = pd.DataFrame(data)
 
-plt.rcParams["figure.figsize"] = [16,9]
-plt.title("S&P500")
-plt.xlabel('Date')
-plt.ylabel('$Price')
-plt.plot(x, y)
+#Not sure I even need to concat these
+#newspindex = pd.merge(df1, sp, left_index=True, right_index=True, how='left')
+#newspindex.index.names = ['Date']
 
+fig, ax = plt.subplots()
+plt.xticks(rotation=90)
+plt.tight_layout()
+
+plt.subplot(2, 1, 1)
+plt.plot(sp.index, sp)
+
+plt.xticks(sp.index[0::80], [])
+
+
+plt.title('S&P500 vs BTC')
+plt.ylabel('% S&P500')
+plt.subplot(2, 1, 2)
+plt.plot(btc.index, btc)
+
+plt.xlabel('year')
+plt.ylabel('% BTC')
+plt.savefig('sp500_btc.png', facecolor='w', edgecolor='w', bbox_inches='tight')
+plt.show()
