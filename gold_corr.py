12929be979ae5313e14f1aa4e04cc202fc5eb7ed
hilljtm <47020214+hilljtm@users.noreply.github.com>
2019-05-23 14:57:11 -0400

    corr plot displaying on sp500 page, cleaned up spindex page, need to save all api calls and convert to csv, sp500 and faang look identical press f

--- gold_corr.py
+++ gold_corr.py
@@ -11,18 +11,27 @@ plt.style.use('seaborn-whitegrid')
 quandl.ApiConfig.api_key = config.api_key
 
 gold = quandl.get("WGC/GOLD_DAILY_USD", start_date='2016-01-05')
-btc = quandl.get("BITFINEX/BTCUSD", column_index='1', start_date='2016-01-05')
+
+
+btc = pd.read_csv("btcprice.csv", index_col='Date', parse_dates=['Dates'])
 df1 = pd.DataFrame(data=gold)
 df = pd.DataFrame(data=btc)
 
+#Export gold to csv, clean NaN columns, merge btc and gold
+gbdf = pd.merge
 
-joinedgbtc = pd.concat([df1, df], axis=1)
-joinedgbtc.to_csv('joinedgbtc.csv')
+def corr(x, y, **kwargs):
+    coef = np.corrcoef(x, y)[0][1]
+    label = r'$\rho$ = ' + str(round(coef, 2))
+
+    ax = plt.gca()
+    ax.annotate(label, xy=(0.2, 0.95), size=10, xycoords=ax.transAxes)
 
 
-def btcgcor():
-    df2 = pd.read_csv('joinedgbtc.csv')
-    df2['btc'].plot()
-    #df2_corr = df2.corr()
+grid = sns.PairGrid(data=df, vars=['GOLD', 'BTC'], size=4)
 
-# joinedgbtc()
+grid = grid.map_upper(plt.scatter, color='darkred')
+gird = grid.map_upper(corr)
+grid = grid.map_lower(sns.kdeplot, cmap='Reds')
+grid = grid.map_diag(plt.hist, bins=10, edgecolor='k', color='darkred')
+
--- spinstats.py
+++ spinstats.py
@@ -4,21 +4,20 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-from app import faang
+from app import spindex
 import os
 import sys
-sys.path.append("path to file")
+sys.path.append("/Users/joeyhill/Desktop/Correlations/spindex.py")
 mpl.rcParams['figure.dpi'] = 300
 sns.set_style('whitegrid', {"axes.facecolor": ".9"})
 
+spdf = pd.read_csv("sp500.csv", index_col='Date', parse_dates=['Date'])
 
-df = pd.read_csv("faangfinal.csv", index_col='Date', parse_dates=['Date'])
+sbtc = pd.read_csv("btcprice.csv", index_col='Date', parse_dates=['Date'])
 
-df1 = pd.read_csv("btcprice.csv", index_col='Date', parse_dates=['Date'])
-
-
-df3 = df.merge(df1, on=['Date'])
-df3.corr(method='pearson')
+spbtc = spdf.merge(sbtc, on=['Date'])
+spbtc.corr(method='pearson')
+spbtc.corr()
 
 
 def corr(x, y, **kwargs):
@@ -29,12 +28,12 @@ def corr(x, y, **kwargs):
     ax.annotate(label, xy=(0.2, 0.95), size=10, xycoords=ax.transAxes)
 
 
-grid = sns.PairGrid(data=df3, vars=['FAANG', 'BTC'], size=4)
+grid = sns.PairGrid(data=spbtc, vars=['Price', 'BTC'], size=4)
 
 grid = grid.map_upper(plt.scatter, color='darkred')
 gird = grid.map_upper(corr)
 grid = grid.map_lower(sns.kdeplot, cmap='Reds')
 grid = grid.map_diag(plt.hist, bins=10, edgecolor='k', color='darkred')
 
-plt.savefig('faangcorco.png', facecolor='w',
+plt.savefig('spincorr.png', facecolor='w',
             edgecolor='w', bbox_inches='tight')
